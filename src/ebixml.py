90bb6e687c7fd174da1532336db166c963c21a1c
Ryan Kelly <kellyrj2@muohio.edu>
2010-06-01 11:00:42 -0400

    initializing repo

---
+++ src/ebixml.py
@@ -0,0 +1,223 @@
+#!/usr/bin/python
+import string, os, sys, time, getopt
+import MySQLdb
+import ConfigParser
+from random import choice
+from xml.sax import ContentHandler
+from xml.sax import make_parser
+from xml.sax.handler import feature_namespaces
+
+class ProteinAnnotation:
+    def __init__(self, id):
+        self.id = id
+        self.length = length
+        self.crc64 = crc64
+    def __getitem__(self):
+        return id
+
+class EBIXML(ContentHandler):
+
+    def __init__(self,session,config=None,outfile=sys.stdout):
+        self.protein = {}
+        self.interpro = {}
+        self.match = {}
+        self.location = {}
+
+        self.class_id = ''
+        self.class_type = ''
+        
+        self.session = session
+
+        self.in_protein = False
+        self.in_interpro = False
+        self.in_match = False
+        self.in_location = False
+        self.in_classification = False
+        self.outfile = outfile
+        if config:
+            self.config = config
+            self.db_con = MySQLdb.connect(host=self.config.get('local db','host'),
+                                    user=self.config.get('local db','user'),
+                                    passwd=self.config.get('local db','passwd'),
+                                    db=self.config.get('local db','db'))
+            self.db_cursor = self.db_con.cursor()
+            self.db_cursor.execute("SELECT MAX(pim_id) FROM protein_interpro_match;")
+            self.pim_id = self.db_cursor.fetchall()[0][0]
+            
+            db_struct = open('structure.sql', 'r')
+            struct_sql = db_struct.read() % ((self.session,)*7)
+            print >> self.outfile, struct_sql
+            self.outfile.flush()
+            
+            if self.pim_id is None:
+                self.pim_id = 1
+        else:
+            self.config = None
+            self.pim_id = 1
+
+    def startElement(self, name, attrs):
+        if name == "protein":
+            self.in_protein = True
+            self.protein = {'id': attrs.get('id',None), 'length': int(attrs.get('length',None)),
+                         'crc64':attrs.get('crc64', None)}
+        elif name in ("interpro", "ipr"):
+            self.in_interpro = True
+            self.interpro = {'id': attrs.get('id', None), 'name': attrs.get('name',None).replace('"',''), 
+                          'type': attrs.get('type',None)}
+        elif name == "match":
+            self.pim_id += 1
+            self.in_match = True
+            self.match = {'id': attrs.get('id',None), 'name': attrs.get('name',None).replace('"',''), 
+                       'dbname': attrs.get('dbname',None)}
+        elif name in ("location", "lcn") and self.in_match:
+            self.in_location = True
+            str_score = attrs.get('score',None)
+            if str_score == "." or str_score == "NA": str_score = "0.0"
+            self.location = { 'start': int(attrs.get('start',None)), 
+                           'end': int(attrs.get('end',None)), 
+                            'score': float(str_score),
+                           'status': attrs.get('status',None),
+                           'evidence': attrs.get('evidence',None)}
+        elif name == "classification":
+            self.in_classification = True
+            self.class_type = attrs.get('class_type', None)
+            self.class_id = attrs.get('id', None)
+    def endElement(self, name):
+        if name == "protein":
+            self.in_protein = False
+            sql_string = 'INSERT INTO `%s_protein` (protein_id, length, crc64, nprot) VALUES ("%s", %d, "%s", %d) ' % (
+                       self.session, self.protein["id"], self.protein["length"], self.protein["crc64"], 1)
+            sql_string += 'ON DUPLICATE KEY UPDATE nprot=nprot+1;'
+            print >> self.outfile, sql_string
+            self.outfile.flush()
+        elif name in ("interpro", "ipr"):
+            self.in_interpro = False
+            sql_string = 'INSERT IGNORE INTO `%s_interpro` (interpro_id, name, ipr_type) VALUES ("%s", "%s", "%s");' % (
+                            self.session, self.interpro['id'], self.interpro['name'], self.interpro['type'])
+            sql_string2 = 'INSERT INTO `%s_protein_interpro` (protein_id, interpro_id) VALUES ("%s", "%s");' % (
+                     self.session, self.protein['id'], self.interpro['id'])
+            print >> self.outfile, sql_string
+            print >> self.outfile, sql_string2
+        elif name == "match":
+            self.in_match = False
+            sql_string = 'INSERT INTO `%s_protein_interpro_match` ' % (
+                     self.session)
+            sql_string += "(pim_id, protein_id, interpro_id, match_id) "
+            
+            sql_string += 'VALUES (%d, "%s", "%s", "%s"); ' % (
+                      self.pim_id, self.protein['id'], self.interpro['id'], self.match['id'])
+            sql_string2 = 'INSERT IGNORE INTO `%s_iprmatch` ' % (
+                     self.session)
+            sql_string2 += "(id, pim_id, name, db_name) "
+
+            sql_string2 += ' VALUES ("%s", %d, "%s", "%s");' % (
+                                self.match['id'], self.pim_id, self.match['name'], self.match['dbname'])
+            print >> self.outfile, sql_string
+            print >> self.outfile, sql_string2
+        elif name in ("location", "lcn"):
+            self.in_location = False
+            sql_string = 'INSERT INTO `%s_location` ' % (self.session)
+            sql_string += "(loc_id, match_id, pim_id, start_p, end_p, score, status, evidence) "
+            sql_string += 'VALUES  (NULL, "%s", %d, %d, %d, %e, "%s", "%s");' % (
+                       self.match['id'],
+                       self.pim_id,
+                       self.location['start'],
+                       self.location['end'],
+                       self.location['score'],
+                       self.location['status'],
+                       self.location['evidence'])
+            print >> self.outfile, sql_string
+        elif name == "classification":
+            self.in_classification = False
+            sql_string = 'INSERT INTO `%s_protein_classification` ' % (self.session)
+            sql_string += "(id, protein_id, class_id, class_type) "
+            sql_string += 'VALUES (NULL, "%s", "%s", "%s");' % (
+                        self.protein["id"], self.class_id, self.class_type)
+            print >> self.outfile, sql_string
+        
+    def endDocument(self):
+        # Update the mysql database
+        if self.outfile is not sys.stdout:
+            self.outfile.flush()
+            os.system("mysql %s -u%s -h %s -p%s < %s" %
+                      (self.config.get('local db','db'), self.config.get('local db','user'),
+                       self.config.get('local db','host'), self.config.get('local db','passwd'),
+                       self.outfile.name))
+            log = open(os.path.join(self.config.get('html','directory'),
+                                    'tbl_creation.log'), 'a')
+            log.write(str(time.ctime()) + "\t" + self.session + "\n")
+
+if __name__ == '__main__':
+    filelist=  []
+    parse_outfile = sys.stdout
+    session = None
+    usage = "Usage: ebixml.py [-c | --config= <config_file>] " + \
+						"[-o | --outfile= <sql_output_file>] [-s | --session= " + \
+						"<unique_identifier>] <xml_file ...>"
+    
+    # 
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"h:c:o:s:",["help","config=","outfile=","session="])
+    except getopt.GetoptError:
+        print usage
+        sys.exit(2)
+    config = None
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            print usage
+            sys.exit(1)
+        elif o in ("-c", "--config"):
+            config = ConfigParser.ConfigParser()
+            config.readfp(open(a))
+        elif o in ("-o", "--outfile"):
+            parse_outfile = a
+        elif o in ("-s", "--session"):
+            session = a
+        else:
+            filelist.append(a)
+    if len(args) < 1:
+        print usage
+        sys.exit(2)
+    elif len(args) > 1 and session:
+        print "Parsing more than one XML file; using random session ids"
+
+    print args
+    fver = 1
+    for filename in args:
+
+        # Create a random session id
+        if len(args) > 1:
+            chars = string.letters + string.digits
+            session = ''.join([choice(chars) for i in xrange(8)])
+               
+        print "doing", filename
+        
+        if config:
+            path = os.path.join(config.get('html','directory'),session)
+        else:
+            path = os.path.join(session)
+            
+        if not(os.path.exists(path)):
+            os.mkdir(path)
+        
+        if parse_outfile is sys.stdout:
+            parse_outfile = os.path.join(path,
+                    os.path.splitext(os.path.basename(filename))[0] + ".iprscan.sql")
+
+        # Create a parser
+        parser = make_parser()
+
+        # Tell the parser we are not interested in XML namespaces
+        parser.setFeature(feature_namespaces, 0)
+
+        # Create the Handler
+        if parse_outfile is not sys.stdout:
+            exh = EBIXML(session, config=config,outfile=open(parse_outfile,"w"))
+        else:
+            exh = EBIXML(session, config=config)
+
+        parser.setContentHandler(exh)
+        parser.parse(filename)
+        print "Done", filename, "Session", session
+        fver += 1
+        del parser
---
+++ src/ipsstatsdata.py
@@ -0,0 +1,169 @@
+#!/usr/bin/python
+import os, ConfigParser
+from pygooglechart import PieChart2D
+from pylab import *
+import MySQLdb
+
+class IPSStatsData:
+    
+    def __init__(self, session, config):
+        self.session = session
+        self.config = config
+        self.go_lookup = self.config.getboolean('general','go_lookup')
+        
+        # Database connections
+        self.conn = self._get_db_connection(config)
+        if self.go_lookup:
+            self.go_conn = self._get_go_db_connection(config)
+            self.go_cursor = self.go_conn.cursor()
+        
+        # Separate cursors for counts and matches
+        self.count_cursor = self.conn.cursor()
+        self.match_cursor = self.conn.cursor()
+        
+        # Current app/match db being used
+        self.current_app = None
+        
+        # Hash of database links
+        self.linkdb = {
+            'PFAM'       :'http://pfam.sanger.ac.uk/family?acc=%s',
+            'PIR'        :'http://pir.georgetown.edu/cgi-bin/ipcSF?id=%s',
+            'HAMAP'      :'http://www.expasy.org/unirule/%s',
+            'PANTHER'    :'http://www.pantherdb.org/panther/family.do?' + 
+                          'clsAccession=%s',
+            'PRINTS'     :'http://www.bioinf.manchester.ac.uk/cgi-bin/' + 
+                          'dbbrowser/PRINTS/DoPRINTS.pl?cmd_a=Display&' + 
+                          'qua_a=/Full&fun_a=Code&qst_a=%s',
+            'PRODOM'     :'http://prodom.prabi.fr/prodom/current/cgi-b' + 
+                          'in/request.pl?question=DBEN&query=%s',
+            'PRO'        :'http://expasy.org/cgi-bin/prosite-search-ac?%s',
+            'SMART'      :'http://smart.embl-heidelberg.de/smart/do_ann' + 
+                          'otation.pl?BLAST=DUMMY&DOMAIN=%s',
+            'SUPERFAMILY':'http://supfam.cs.bris.ac.uk/SUPERFAMILY/cgi-' + 
+                          'bin/scop.cgi?sunid=%s',
+            'TIGRFAMs'   :'http://cmr.jcvi.org/cgi-bin/CMR/HmmReport.cgi' + 
+                          '?hmm_acc=%s',
+            'GENE3D'     :'http://www.cathdb.info/gene3d/%s',
+            'GO'         :'http://www.ebi.ac.uk/QuickGO/GTerm?id=%s#ancchart'}
+    
+    # Private method to retrieve the local database connection given the configuration
+    # Throws a connection error if it cannot connect
+    def _get_db_connection(self, config):
+        return MySQLdb.connect(host=self.config.get('local db', 'host'),
+            user=self.config.get('local db','user'), passwd=self.config.get('local db','passwd'),
+            db = self.config.get('local db','db'))
+    
+    # Private method to retrieve GO Term database connection
+    # Throws a connection error if it cannot connect
+    def _get_go_db_connection(self, config):
+        return MySQLdb.connect(host=self.config.get('go db', 'host'),
+            user=self.config.get('go db','user'), passwd=self.config.get('go db','passwd'),
+            port=self.config.getint('go db', 'port'), db = self.config.get('go db','db'))
+    
+    # Get a list of (name, count) pairs for a given app/match db
+    # Returns [(name1, name2, name3), (count1, count2, count3)] or None
+    def get_counts(self, app, limit=35):
+        count_by_name = []
+        self.count_cursor.execute("SELECT name, count(1) as count FROM %s_iprmatch " % (self.session) + 
+                        "WHERE db_name ='%s' GROUP BY name " % (app) + 
+                        "ORDER BY count DESC LIMIT %s" % (limit))
+        for row in self.count_cursor:
+            count_by_name.append((int(row[1]), (row[0])))
+        
+        if len(count_by_name) > 0:
+            return zip(*count_by_name)
+        else:
+            return None
+    
+    # Generate a chart given [(label1, label2), (value1, value2)] data
+    # Returns True or False depending on if the chart was generated
+    def get_chart(self, chart_data, chart_title, chart_filename, google=True):
+        if chart_data:
+            [count, labels] = chart_data
+            if google:
+                chart = PieChart2D(730, 300)
+                chart.set_colours(('66FF66', 'FFFF66', '66FF99'))
+                chart.set_title(chart_title)
+                chart.add_data(list(count))
+                chart.set_pie_labels(list(labels))
+                try:
+                    chart.download(chart_filename)
+                    chart_generated = True
+                except:
+                    chart_generated = False
+            if not google or not chart_generated:
+                try:
+                    figure(figsize=(7, 2), dpi=150)
+                    axis('scaled')
+                    _, tlabels = pie(count, labels=labels, shadow=False)
+                    for label in tlabels:
+                        label.set_size(9)
+                    title(chart_title, fontsize=12)
+                    savefig(chart_filename)
+                    return True
+                except:
+                    return False
+    
+    # Sets the match cursor for iterating through the matches
+    def init_match_data(self, app, limit=35):
+        self.current_app = app
+        self.match_cursor.execute("""select I.name, I.id, class_id, M.count
+            from 
+                (%s_iprmatch as I join %s_protein_interpro_match as P 
+                on I.pim_id = P.pim_id join %s_protein_classification
+                as C on P.protein_id = C.protein_id join (select name,
+                count(1) as count from %s_iprmatch where db_name = '%s'
+                group by name order by count desc limit %s) as M on
+                I.name = M.name)
+            where db_name = '%s'
+            group by class_id
+            order by M.count DESC, name ASC;""" % (self.session, self.session, self.session, self.session, app, limit, app))
+
+    # Get the raw database results from the match data query
+    # Returns (Name, DB_ID, GO_ID, Count) or None
+    def get_raw_match_data_row(self):
+        return self.match_cursor.fetchone()
+    
+    # Appends the database url and GO url to the raw match data
+    # Returns (DB_Name, DB_URL, Count, GO_Name, GO_URL) or None
+    def get_link_data_row(self):
+        row = self.get_raw_match_data_row()
+        if row:
+            name, db_id, go_id, count = row
+            if self.current_app in self.linkdb.keys():
+                db_url = self.linkdb[self.current_app] % db_id
+            else:
+                db_url = ''
+            go_url = self.linkdb['GO'] % go_id
+            
+            if self.go_lookup:
+                go_info = self.retrieve_go_info(go_id)
+                return name, db_url, count, go_info[0], go_url, go_info[1]
+            else:
+                return name, db_url, count, go_id, go_url
+        else:
+            return None
+    
+    # Retrieve the GO Term name and definition; slow without local installation
+    # Returns (GO_Name, GO_Definition) given GO_ID
+    def retrieve_go_info(self, go_id):
+        self.go_cursor.execute(
+            'select name, term_definition from term join term_definition' + \
+            ' on id = term_id where acc = "%s"' % (go_id))
+        goinfo = self.go_cursor.fetchone()
+            
+        return goinfo
+    
+    # Closes connections to the databases
+    def close(self):
+        self.conn.close()
+
+if __name__ == '__main__':
+    config = ConfigParser.ConfigParser()
+    config.readfp(open('ipsstats.cfg'))
+    i = IPSStatsData('Xs7O4pYH',config)
+    i.init_match_data('TIGRFAMs')
+    while True:
+        row = i.get_link_data_row()
+        if not(row): break
+    
